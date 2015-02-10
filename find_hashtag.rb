21187e546c05d6eb956dd4056d0390dc0f22b4bc
axton21 <axton_n@yahoo.com>
2015-02-10 01:28:09 -0500

    I don't got no type

--- find_hashtag.rb
+++ find_hashtag.rb
@@ -1,3 +1,15 @@
+require "pry"
+
 def get_hashtags(tweet)
-  #code here
+    hashtags = []
+    tags = []
+    word = []
+    array = tweet.split(" ")
+
+tags = array.grep(/^(#)[^\d]\w+$/i)
+     tags.each do |tag|
+       word = tag.gsub('#', '')
+      hashtags << word
+    end
+hashtags
 end
